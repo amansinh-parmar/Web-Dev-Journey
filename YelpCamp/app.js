2d96f6c802001cf8a0b0097c4d025db1353ddae3
amansinh-30 <amansinhparmar30@gmail.com>
2025-08-31 23:43:28 +0530

    Create camping site project

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -24,7 +25,7 @@ db.once("open", () => {
 // =============== Get "ejs" Templates ===============
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.engine("ejs", ejsMate);
 // use middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -11,6 +11,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
   useUnifiedTopology: true,
 });
 
+// mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
 // check there is an error or not
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -18,15 +20,25 @@ db.once("open", () => {
   console.log("Database Connected!!");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// API Key for fetch images
+// const PEXELS_API_KEY =
+//   "qf4WkaFrva8Ktd5ydWNa6hb9hvEAcFG9clJo8SLDVOQpZSyhObEeQqF6";
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 1; i < 50; i++) {
     const randomIndex = Math.floor(Math.random() * 500);
+    const price = Math.floor(Math.random() * 20) + 10;
+    // const image = await fetchCampingImage(); // get a fresh image for each campground
     const camp = new Campground({
       location: `${cities[randomIndex].city}, ${cities[randomIndex].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto ex ducimus libero architecto, nulla sunt.",
+      price,
     });
     await camp.save();
   }
