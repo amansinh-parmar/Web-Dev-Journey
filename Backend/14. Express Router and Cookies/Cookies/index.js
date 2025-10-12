8e4d88bee70471ba7f81c5332d537bfcba0b27dd
amansinh-30 <amansinhparmar30@gmail.com>
2025-10-12 15:52:38 +0530

    express cookies

--- Backend/14. Express Router and Cookies/Cookies/index.js
+++ Backend/14. Express Router and Cookies/Cookies/index.js
@@ -1,10 +1,34 @@
+// =============== Import Modules ===============
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
+// =============== Middleware ===============
+app.use(cookieParser("ThisIsYourSecret"));
+
+// =============== Routes ===============
 app.get("/greet", (req, res) => {
-  res.send("HEYY, THERE..!!");
+  const { name = Anonyoums } = req.cookies;
+  res.send(`Heyy there, ${name}`);
 });
 
+app.get("/setname", (req, res) => {
+  res.cookie("name", "APEX");
+  res.cookie("animal", "Tiger");
+  res.send("OKAY, SET YOU A COOKIE!!");
+});
+
+app.get("/getsingedcookie", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true });
+  res.send("Okay, Signed your 'Fruit Cookie'!! ");
+});
+
+app.get("/verifyfruit", (req, res) => {
+  console.log(req.cookies);
+  res.send(req.signedCookies);
+});
+
+// =============== Listen Port ===============
 app.listen(3000, () => {
   console.log("Express Server Login Port:3000");
 });
