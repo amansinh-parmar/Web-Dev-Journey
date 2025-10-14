6a375dd88a69fa99907725cb7012eae36433c92d
amansinh-30 <amansinhparmar30@gmail.com>
2025-10-14 21:24:10 +0530

    authentication use middleware

--- Backend/16. Authentication/AuthDemo/index.js
+++ Backend/16. Authentication/AuthDemo/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./models/user");
+const session = require("express-session");
 
 // ================ Middleware ================
 mongoose
@@ -21,6 +22,17 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({ secret: "notagoodsecret", saveUninitialized: false, resave: false })
+);
+
+// check for user this session to use middleware
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
 
 // ================ Routes ================
 // 'HOME PAGE'
@@ -37,30 +49,52 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   // Store password in "Hash Formation"
-  const hash = await bcrypt.hash(password, 12);
-  const user = new User({
-    username,
-    password: hash,
-  });
+  // ==>> Note: Use this line to get password from user do hashing function and then save in model/mongoose.
+  // const hash = await bcrypt.hash(password, 12);
+  // const user = new User({ username, password: hash });
+
+  // ==>> Note: Use this line we need middleware in 'user.js' model file.
+  const user = new User({ username, password });
   await user.save();
   //   res.send(hash);
   console.log(user);
+  req.session.user_id = user._id;
   res.redirect("/");
 });
 
+// ================ Login Page ================
 app.get("/login", (req, res) => {
   res.render("login");
 });
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    res.send("YAY WELCOME!");
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    res.redirect("/secret");
   } else {
-    res.send("OOPS, TRY AGAIN!!");
+    res.redirect("/login");
   }
   // res.render("login");
+});
+
+// ================ Logout Page ================
+app.post("/logout", (req, res) => {
+  // req.session.user_id = null;
+  req.session.destroy(); //use this to destroy the session entirely
+  res.redirect("/login");
+});
+
+// ================ Secret Page ================
+// use middleware 'requireLogin'
+app.get("/secret", requireLogin, (req, res) => {
+  // res.send("THIS IS SECRET! YOU CAN'T SEE ME UNLESS YOU ARE LOGGED IN!!");
+  res.render("secret");
+});
+
+// use middleware 'requireLogin'
+app.get("/topsecret", requireLogin, (req, res) => {
+  res.send("This is the 'TOP SECRET' to come here..!!");
 });
 // ================ Server Port Login ================
 app.listen(8080, () => {
--- Backend/16. Authentication/AuthDemo/models/user.js
+++ Backend/16. Authentication/AuthDemo/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 // ============== Create Schema ==============
 const userSchema = new Schema({
@@ -11,6 +12,20 @@ const userSchema = new Schema({
     type: String,
     required: [true, "Password cannot be blank"],
   },
+});
+
+// ============== Middleware ==============
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username });
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  return isValid ? foundUser : false;
+};
+
+// Replace the password with using 'middleware' so nobody can see inside the database as well.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 // ============== Export Schema ==============
