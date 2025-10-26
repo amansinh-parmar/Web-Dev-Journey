90a512f9ec6f5e075996978a140474c3c7096728
amansinh-30 <amansinhparmar30@gmail.com>
2025-10-26 18:55:52 +0530

    update campground file for routes

--- YelpCamp/routes/campground.js
+++ YelpCamp/routes/campground.js
@@ -18,26 +18,43 @@ const campground = require("../controllers/campground");
 
 const multer = require("multer");
 const { storage } = require("../cloudinary");
-const upload = multer({ storage });
+const upload = multer({ dest: storage });
 // =============== Validation Middleware ===============
 
 // =============== Routes ===============
 
 // =============== Easy Way To Write Routes ===============
-router.route("/")
-.get(catchAsync(campground.index))
-.post(isLoggedIn,upload.array("image"),validateCampground,catchAsync(campground.createCampground));
+router
+  .route("/")
+  .get(catchAsync(campground.index))
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campground.createCampground)
+  );
 // also use "single" to get Single Image and "array" to get multiple images
 
 router.get("/new", isLoggedIn, campground.randerNewForm);
 
-router.route("/:id")
-.get(catchAsync(campground.showCampground))
-.put(isLoggedIn, isAuthor,upload.array("image"),validateCampground,catchAsync(campground.updateCampground))
-.delete(isLoggedIn,isAuthor,catchAsync(campground.deleteCampground))
+router
+  .route("/:id")
+  .get(catchAsync(campground.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campground.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campground.deleteCampground));
 
-router.get("/:id/edit",isLoggedIn,isAuthor, catchAsync(campground.randerEditForm));
-
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campground.randerEditForm)
+);
 
 module.exports = router;
 
